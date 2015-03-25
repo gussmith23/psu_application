8d405a699bc60b4e96201305bbdc25ea49de7499
M-Porter <mtp5129@gmail.com>
2015-03-25 00:35:43 -0400

    updated

--- app/routes/catchAll.php
+++ app/routes/catchAll.php
@@ -7,3 +7,9 @@
 $app->get('/:method', function($method) use ($app) {
     $app->render('app.php');
 })->conditions(array('method' => '.+'));
+
+
+$app->options('/:method', function($method) use ($app) {
+    $app->response->header('Access-Control-Allow-Origin', '*');
+    $app->response->header('Access-Control-Allow-Headers', 'X-Requested-With, X-Authentication, X-client, X-authentication'); //Allow JSON data to be consumed
+})->conditions(array('method' => '.+'));
