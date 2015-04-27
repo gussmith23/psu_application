396f82b7f868dccad09071142532a5c205c68ba0
M-Porter <mtp5129@gmail.com>
2015-04-27 12:54:49 -0400

    removed CORS stuff

--- public/index.php
+++ public/index.php
@@ -7,11 +7,11 @@ require '../vendor/autoload.php';
 $app = new \Slim\Slim();
 $app->add(new \Slim\Middleware\SessionCookie(array('secret' => uniqid(rand(), true))));
 
-$app->options('/(:name+)', function() use ($app) {
-    $app->response->header('Access-Control-Allow-Origin', '*');
-    $app->response->header('Access-Control-Allow-Methods', 'PUT, DELETE, POST, GET');
-    $app->response->header('Access-Control-Allow-Headers', 'Authorization, X-Authorization');
-});
+// $app->options('/(:name+)', function() use ($app) {
+//     $app->response->header('Access-Control-Allow-Origin', '*');
+//     $app->response->header('Access-Control-Allow-Methods', 'PUT, DELETE, POST, GET');
+//     $app->response->header('Access-Control-Allow-Headers', 'Authorization, X-Authorization');
+// });
 
 // config
 require '../app/configs/config.php';
